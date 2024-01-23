ac7ecb6fcbbf197a52924b11675a3d69cbfbc6f2
piyushmore8227 <piyushmore8227@gmail.com>
2024-01-23 13:56:30 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -23,41 +23,26 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-    # try:
-    #     report = {}
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
 
-    #     for i in range(len(list(models))):
-    #         model = list(models.values())[i]
-    #         para=param[list(models.keys())[i]]
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
 
-    #         gs = GridSearchCV(model,para,cv=3)
-    #         gs.fit(X_train,y_train)
+            model.fit(X_train, y_train)  # Train model
 
-    #         model.set_params(**gs.best_params_)
-    #         model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
 
-    #         #model.fit(X_train, y_train)  # Train model
+            y_test_pred = model.predict(X_test)
 
-    #         y_train_pred = model.predict(X_train)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-    #         y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-    #         train_model_score = r2_score(y_train, y_train_pred)
+            report[list(models.keys())[i]] = test_model_score
 
-    #         test_model_score = r2_score(y_test, y_test_pred)
+        return report
 
-    #         report[list(models.keys())[i]] = test_model_score
-
-    #     return report
-
-    # except Exception as e:
-    #     raise CustomException(e, sys)
-    
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
-
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
